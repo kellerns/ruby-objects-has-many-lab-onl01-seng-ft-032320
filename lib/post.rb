815e99f9a3d02217f65193dd1c8e15226e5b45cb
Nicholas Keller <nic.keller328@gmail.com>
2020-04-06 19:04:06 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,26 @@
 class Post
+  attr_accessor :title, :author
   
+  @@all = []
   
+  def initialize(name)
+    @name = name
+    save
+  end
   
+  def self.all
+    @@all
+  end
   
+  def author
+    @author
+  end
   
+  def author_name
+      @author.name
+  end
   
+  def save
+    @@all << self
+  end
 end
