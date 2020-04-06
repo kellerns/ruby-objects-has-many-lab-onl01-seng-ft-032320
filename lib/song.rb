7a5b01e70bcc6a97c30aaa036d829b5d15b686ce
Nicholas Keller <nic.keller328@gmail.com>
2020-04-06 18:28:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,23 @@
 class Song
   attr_accessor :name, :artist
   
+  @@all = []
+  
   def initialize(name)
     @name = name
   end
   
+  def self.all
+    @all
+  end
   
+  def artist
+    @artist
+  end
+  
+  def artist_name
+    @artist.name
+  end
   
   
 end
