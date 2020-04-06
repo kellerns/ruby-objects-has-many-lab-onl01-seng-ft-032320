591b8e008018d37a8f745dbf6e6a0f1e7a7c1e3f
Nicholas Keller <nic.keller328@gmail.com>
2020-04-06 18:59:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,21 @@
 class Artist
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
-    @songs = []
   end
   
+  def add_song(song)
+    song.artist = self
+  end
+
   def songs
-    @songs
-  end
-  
-  def add_song(new_song)
-    @songs << new_song
-    new_song.artist = self
-  end
+    Song.all.select {|song| song.artist == self}
+  end  
   
   def add_song_by_name(song_name)
-    new_song = Song.new(song_name)
-    add_song(new_song)
+    song = Song.new(song_name)
+    add_song(song)
   end
   
   def self.song_count
